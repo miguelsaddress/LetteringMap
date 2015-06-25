7433650e5ecabe2555a59b69566fee278a45a4cc
miguel <miguel@rentalia.com>
2015-06-25 16:11:19 +0200

    extracting token to a file

--- instagram.php
+++ instagram.php
@@ -1,7 +1,6 @@
 <?php
+include_once '.token';
 include_once 'classes/InstagramFetcher.php';
-
-$ACCESS_TOKEN = "31961161.e68fb44.07ddce39983f498489798b87440d3f89";
 
 $fetcher = new InstagramFetcher($ACCESS_TOKEN, "goldensecretsoflettering");
 $fetcher->fetch();
